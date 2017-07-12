5921ca4c96b405586505d8b01cc8f5a61cfdfc03
Boksha Anton <antonboksha@gmail.com>
2017-07-12 21:03:02 +0300

    change database from local to mlab on heroku

--- config/default.js
+++ config/default.js
@@ -1,7 +1,7 @@
 module.exports = {
     // Database settings
     mongoose: {
-        uri: 'mongodb://localhost/eleken',
+        uri: 'mongodb://heroku_7nj9hjsl:uj3qd3a7sqo8sob5uh2d1nolln@ds143181.mlab.com:43181/heroku_7nj9hjsl',
         options: {
             server: {
                 socketOptions: {
